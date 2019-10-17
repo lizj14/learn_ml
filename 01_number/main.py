05892bb669b058a47642642d9629083ab1a10664
lizijie <lizj14@outlook.com>
2019-10-17 20:34:23 +0800

    changing

--- 01_number/main.py
+++ 01_number/main.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import gzip
+import time
+import sys
 
 IMAGE_SIZE = 28
 NUM_CHANNELS = 1
@@ -9,6 +11,7 @@ VALIDATION_SIZE = 5000
 BATCH_SIZE = 64
 EVAL_BATCH = 64
 NUM_LABELS = 10
+EVAL_FREQUENCE = 10
 
 def data_type():
     return np.float64
@@ -77,10 +80,77 @@ def main(_):
                 ksize = [1,2,2,1],
                 strides=[1,2,2,1],
                 padding='SAME')
-        pool_shape = pool.get_shape().as_list()
-        reshape = tf.reshape(pool,
+        pool_shape = pool1.get_shape().as_list()
+        reshape = tf.reshape(pool1,
                 [pool_shape[0], pool_shape[1]*pool_shape[2]*pool_shape[3]])
         return tf.matmul(reshape, fc1_weights) + fc1_bias
+
+    logits = model(train_data, True)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        labels=train_label, logits=logits))
+
+    regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_bias))
+    loss += 5e-4 * regularizers
+
+    batch = tf.Variable(0, dtype=data_type())
+    learning_rate = tf.train.exponential_decay(
+            0.01,                # base rate
+            batch * BATCH_SIZE,  # current index
+            train_size,
+            0.95,                # decay rate
+            staircase=True)
+
+    optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(
+            loss, global_step=batch)
+
+    train_prediction = tf.nn.softmax(logits)
+    eval_prediction = tf.nn.softmax(model(eval_data))
+
+    def eval_in_batches(data, sess):
+        size = data.shape[0]
+        if size < EVAL_BATCH:
+            raise ValueError("batch size larger than dataset: %d" % size)
+        predictions = np.ndarray(shape=(size, NUM_LABELS), dtype=np.float64)
+        for begin in range(0, size, EVAL_BATCH):
+            end = begin + EVAL_BATCH
+            if end <= size:
+                predictions[begin:end, :] = sess.run(
+                        eval_prediction,
+                        feed_dict={eval_data: data[begin:end, ...]})
+            else:
+                batch_predictions = sess.run(
+                        eval_prediction,
+                        feed_dict={eval_data: data[-EVAL_BATCH:, ...]})
+                predictions[begin:, :] = batch_predictions[begin-size:, :]
+        return predictions
+
+    start_time = time.time()
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        print('init')
+        for step in range(int(num_epoch * train_size) // BATCH_SIZE):
+            offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
+            batch_data = train_data[offset:(offset + BATCH_SIZE), ...]
+            batch_label = train_label[offset:(offset+BATCH_SIZE)]
+            feed_dict = {train_data: batch_data,
+                    train_label: batch_label}
+            sess.run(optimizer, feed_dict=feed_dict)
+            if step % EVAL_FREQUENCE == 0:
+                l, lr, predictions = sess.run([loss, learning_rate, 
+                    train_prediction], feed_dict=feed_dict)
+                elapsed_time = time.time() - start_time
+                print('step %d (epoch %.2f), %.1f ms' % (step, 
+                    float(step)*BATCH_SIZE / train_size,
+                    1000 * elapsed_time / EVAL_FREQUENCE))
+                print('Minibatch loss: %.3f, learning rate: %.6f' % (l, lr))
+                print('Minibatch error: %.1f%%' % error_rate(predictions, batch_label))
+                print('Validation error: %.1f%%' % error_rate(
+                    eval_in_batches(validation_data, sess), validation_label))
+                sys.stdout.flush()
+
+        test_error = error_rate(eval_in_batches(test_data, sess), test_label)
+        print('Test error: %.1f%%' % test_error)
+        
 
 if __name__ == '__main__':
     tf.app.run(main=main)
